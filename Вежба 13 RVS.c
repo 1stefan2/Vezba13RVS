51fc78065539cfc2b5422e2b1d37bdd38770a952
Stefan <0stefanmitrovic@gmail.com>
2026-05-16 12:31:38 +0200

    Create Вежба 13 RVS.c

---
+++ Вежба 13 RVS.c
@@ -0,0 +1,10 @@
+﻿// Вежба 13 RVS.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
